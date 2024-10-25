2a56d69d1c96823c9dbc4703537bc58942ac266c
Furkan Abay <furkan_abay@hotmail.com>
2024-10-25 22:42:11 +0200

    feat: added mobile responsive

--- src/components/molecules/DescriptionForm.tsx
+++ src/components/molecules/DescriptionForm.tsx
@@ -27,15 +27,15 @@ export default function DescriptionForm({ setUserQuery }: { setUserQuery: Dispat
     <>
       <form onSubmit={handleSubmit(submitInput)} className="mt-14">
         <div className="w-full flex items-center border-[2px] border-[#9F9D9C] rounded-[11px] p-3 relative">
-          <span className="material-symbols-rounded text-[#9F9D9C] !text-[38px] pr-3">
+          <span className="material-symbols-rounded text-[#9F9D9C] !text-[24px] md:!text-[38px] pr-1 md:pr-3">
             search
           </span>
           <input
             autoComplete="off"
             {...register("description", { required: "Skriv en beskrivning om bilen du söker!" })}
             placeholder="Vilken bil är du ute efter?"
-            className="text-2xl font-normal w-full outline-none bg-transparent" />
-          <button type="submit" className="w-[100px] bg-[#0071EB] text-white rounded-[11px] absolute right-2 h-[80%] hover:opacity-80">Sök</button>
+            className="text-base md:text-2xl font-normal w-full outline-none bg-transparent" />
+          <button type="submit" className="w-[70px] md:w-[100px] bg-[#0071EB] text-white rounded-[11px] absolute right-2 h-[80%] hover:opacity-80">Sök</button>
         </div>
         {errors.description && <p className="text-[#ef4050] absolute">{errors.description.message}</p>}
       </form>
